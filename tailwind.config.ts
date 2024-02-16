fbdfce40fc8c2121887aadac6c7f0ede190a9d22
Joseph Cardarelli <jrcardarelli@wpi.edu>
2024-02-16 17:23:07 -0500

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'eggshell': '#EDE4CC',
+      'background': '#FFFFFF',
+      'normal-text': '#42322D',
+      'text-highlight': '#EB6B34',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
